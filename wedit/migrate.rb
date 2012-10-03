bfb038d47ea676f2d59d08b677f61da8d8853098
Brenton 'B-Train' Fletcher <i@bloople.net>
2012-10-03 22:45:14 +1000

    Migrate

--- wedit/migrate.rb
+++ wedit/migrate.rb
@@ -1,14 +1,18 @@
 require 'rubygems'
-require 'mysql2'
+require 'nokogiri'
 require 'memcache'
 
-sql = Mysql2::Client.new(:host => "localhost", :username => "root")
+doc = Nokogiri::XML.parse(File.read("temp.xml"))
 mem = MemCache.new('localhost:21201', :check_size => false)
 
-sql.query("USE wedit")
-results = sql.query("SELECT * FROM documents")
-results.each do |row|
+doc.css("row").each do |row|
+  key = row.css("field[name=key]").text
+  mem.set("wedit-document-#{key}", row.css("field[name=text]").text)
+  mem.set("wedit-document-#{key}-last-modified", row.css("field[name=last_modified]").text)
+end
+=begin
+resiults.each do |row|
   mem.set("wedit-document-#{row["key"]}", row["text"])
   mem.set("wedit-document-#{row["key"]}-last-modified", row["last_modified"])
 end
-
+=end
